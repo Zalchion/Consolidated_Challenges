563409366c97836539d5a9bd7fd81301ae3eb213
Zalchion <zalchion@gmail.com>
2016-05-09 19:10:39 +0200

    Challenge 003, made "StartChallenge" so it starts dynamically

---
+++ Consolidated_Challenges/Consolidated_Challenges/Challenge003.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolidated_Challenges
+{
+	public class Challenge003
+	{
+		public void Challenge_003()
+		{
+			Console.WriteLine("String to cipher and uncipher.");
+			string a = Console.ReadLine();
+			string b = Cipher(a, 18);
+			string c = Cipher(b, -18);
+
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(c);
+
+			Console.WriteLine("Wish to open the challenge?");
+			string input = Console.ReadLine();
+			if(input == "yes" || input == "y")
+				System.Diagnostics.Process.Start("https://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/");
+		}
+
+		public static string Cipher(string a, int shift)
+		{
+			char[] test = a.ToLower().ToCharArray();
+			for(var i = 0; i < test.Length; i++)
+			{
+				char letter = test[i];
+
+				if(char.IsLetter(letter))
+				{
+					letter = (char)(letter + shift);
+
+					if(letter > 'z')
+					{
+						letter = (char)(letter - 26);
+					}
+					else if(letter < 'a')
+					{
+						letter = (char)(letter + 26);
+					}
+				}
+				test[i] = letter;
+			}
+			return new string(test);
+		}
+	}
+}
--- Consolidated_Challenges/Consolidated_Challenges/Program.cs
+++ Consolidated_Challenges/Consolidated_Challenges/Program.cs
@@ -1,74 +1,77 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
-
 namespace Consolidated_Challenges
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{	
+		{
 			while(true)
 			{
-				Console.WriteLine("Write the number of the challenge you wish to view IE ('001' or '077').\n '!help' for commands or '!quit' to quit");
+				Console.WriteLine("Write the number of the challenge you wish to view IE ('001' or '077').\n 'help' for commands or 'quit' to quit");
 				string input = Console.ReadLine();
 				input = input.ToLower().Trim();
 
-				Console.WriteLine(input);
-
-				if(input == "!exit" || input == "!quit" || input == "!q")
+				if(input == "exit" || input == "quit" || input == "q")
 					break;
-				if(input.Length > 0 && input[0] == '!')
+				if(input.Length > 0 && !char.IsDigit(input[0]))
 				{
 					switch(input)
 					{
-						case "!list":
-							//TODO: loops through all challenges and show them.
+						case "list":
+							//Loops through all challenges and show them.
+							string nameSpace = "Consolidated_Challenges";							
+							var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+									where t.IsClass && t.Namespace == nameSpace && t.Name.Substring(0, 3) == "Cha"
+									select t;
+							q.ToList().ForEach(t => Console.WriteLine(t.Name));
 							break;
-						case "!clear":
+						case "clear":
 							Console.Clear();
-							Console.WriteLine("Write the number of the challenge you wish to view.\n '!help' for commands");
 							break;
 						default:
-							Console.WriteLine("Commands: !list, !clear, !quit, !exit");
+							Console.WriteLine("\nThe command '" + input + "' does not exist.\n");
 							break;
 					}
+				}
+				else if(input.Length > 0 && char.IsDigit(input[0]))
+				{
+					string myClass = "Challenge" + input;
+					string myMethod = "Challenge_" + input;
+					StartChallenge(myClass, myMethod);
 				}
 				else
-				{
-					switch(input)
-					{
-						case "001":
-							var c001 = new Consolidated_Challenges.Challenge001();
-							c001.Challenge_001();
-							break;
-						case "002":
-							var c002= new Consolidated_Challenges.Challenge002();
-							c002.Challenge_002();
-							break;
-						default:
-							Console.WriteLine(input + " does not exist.");
-							break;
-					}
-
-					
-					
-				}
-
+					Console.WriteLine("Commands: list, clear, quit, exit or a three digit number\n");
 			}
 
 			Console.WriteLine("Press any key to exit");
 			Console.ReadLine();
 		}
 
+		public static void StartChallenge(string myClass, string myMethod)
+		{			
+			try
+			{
+				//Gets the challenge class and starts it unless an error is thrown
+				Type type = Type.GetType("Consolidated_Challenges." + myClass);
+				MethodInfo methodInfo = type.GetMethod(myMethod);
+				Object obj = Activator.CreateInstance(type);
+				methodInfo.Invoke(obj, null);
+			}
+			catch(Exception)
+			{
+				//The challenge number did not exist
+				Console.WriteLine(myClass + " does not exist. type 'list' to see the list of all challenges\n");
+			}
+		}
 	}
 }
-
-
 
 /*
  			Console.WriteLine("Wish to open the challenge?");
